e6551ef90fda016c0d576e846cbe13c7db5e169a
mnavinc <mnavinc@hotmail.com>
2017-10-23 01:59:28 +0530

    Done the UI part

--- client/js/main.js
+++ client/js/main.js
@@ -12,3 +12,19 @@ $(function(){
         $(this).parent('label').siblings('span').html(filename);
     });
 });
+
+$(".sr-only").click(function(){
+    $(".navbar-toggle").toggleClass("active");
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.right = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.right = "0";
+    document.body.style.backgroundColor = "white";
+}
